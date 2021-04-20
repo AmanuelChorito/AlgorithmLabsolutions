88c7dc294b64753466f7105527784c90a434bef1
AmanuelChorito <amye.end@gmail.com>
2021-04-20 12:02:16 -0500

    new class second smallest element added

---
+++ src/problem2/SecondSmallestElement.java
@@ -0,0 +1,13 @@
+package problem2;
+
+public class SecondSmallestElement {
+
+    public static int secondSmallest(int[] arr) {
+        if(arr==null || arr.length < 2) {
+            throw new IllegalArgumentException("Input array too small");
+        }
+//implement
+        return 1;
+    }
+
+}
