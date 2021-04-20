e6e5f7d733f6d92468e743294e2f78305cf0dd4f
AmanuelChorito <amye.end@gmail.com>
2021-04-20 13:13:14 -0500

    new class second smallest element added

--- src/problem2/SecondSmallestElement.java
+++ src/problem2/SecondSmallestElement.java
@@ -6,8 +6,31 @@ public class SecondSmallestElement {
         if(arr==null || arr.length < 2) {
             throw new IllegalArgumentException("Input array too small");
         }
-//implement
-        return 1;
+        int first=Integer.MAX_VALUE, second=Integer.MAX_VALUE;
+        for (int i=0;i<arr.length;i++){
+              if(arr[i]<=first){
+                   second=first;
+                    first=arr[i];
+                }
+              if(arr[i]!=first&& arr[i]<second)
+                second=arr[i];
+
+
+       }
+
+        return second;
+    }
+
+    public static void main(String[] args) {
+        System.out.println( secondSmallest(new int[]{1,4,2,3}));
+        System.out.println( secondSmallest(new int[]{3,3,4,7}));
+
+
+        System.out.println( secondSmallest(new int[]{3,5,4,7}));
+        System.out.println( secondSmallest(new int[]{3,5,4,7,3}));
+        System.out.println( secondSmallest(new int[]{3,5,4,7,6}));
+
+        System.out.println( secondSmallest(new int[]{9}));
     }
 
 }
