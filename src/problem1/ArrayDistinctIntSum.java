566604e5384dd8a9d6b33608bf2593fecc0bec40
AmanuelChorito <amye.end@gmail.com>
2021-04-21 17:10:23 -0500

    new class subset

--- src/problem1/ArrayDistinctIntSum.java
+++ src/problem1/ArrayDistinctIntSum.java
@@ -7,11 +7,11 @@
             }
             for (int i = 0; i < arr.length - 1; i++) {
                 for (int j = i + 1; j < arr.length; j++) {
-                    if (arr[i] == arr[j]) return false;
-                    if (arr[i] + arr[j] == z) {
-                        return checkDistinct(arr);
-
-                    }
+                    if (arr[i] + arr[j] == z && arr[i]!=arr[j]) return true;
+//                    if (arr[i] + arr[j] == z) {
+//                        return checkDistinct(arr);
+//
+//                    }
                 }
 
             }
--- src/problem1/SumofDistinctImproved.java
+++ src/problem1/SumofDistinctImproved.java
@@ -1,6 +1,18 @@
 package problem1;
 
-public class SumofDistinctImproved {
+import java.util.HashMap;
 
+public class SumofDistinctImproved {
+ public static boolean SumExists( int[] arr, int z){
+     HashMap<Integer,Integer> hashMap= new HashMap();
+     for (int i:arr)
+     hashMap.put(z-i,1);
+     for (int j=0;j< arr.length;++j){
+         if(hashMap.containsKey(arr[j]) && arr[j]!=(z-arr[j])){
+             return true;
+         }
+     }
+     return false;
+ }
 
 }
